006b211c90c8ab88443f69dcc766e47a5706ac11
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-09-08 11:35:19 +0530

    Update BMS_Temperature_Sensor.py

--- BMS_Temperature_Sensor.py
+++ BMS_Temperature_Sensor.py
@@ -1,1 +1,11 @@
+import random
+from random import randint
 
+number_of_samples = 50
+min_Temp_in_Celsius = 0
+max_Temp_in_Celsius = 45
+def Temp_sensor_data_generator(number_of_samples,min_Temp_in_Celsius,max_Temp_in_Celsius):
+    Temp_samples = []
+    for i in range(number_of_samples):
+        Temp_samples.append(random.randint(min_Temp_in_Celsius,max_Temp_in_Celsius))
+    return Temp_samples
