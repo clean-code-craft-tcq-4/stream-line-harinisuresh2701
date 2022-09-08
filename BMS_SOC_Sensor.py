047e8065fb9cec553eba2e6c2e9741bf0a0ec263
harinisuresh2701 <105203673+harinisuresh2701@users.noreply.github.com>
2022-09-08 11:35:41 +0530

    Update BMS_SOC_Sensor.py

--- BMS_SOC_Sensor.py
+++ BMS_SOC_Sensor.py
@@ -7,5 +7,5 @@ max_SOC_value_in_percent = 100
 def SOC_sensor_data_generator(number_of_samples,min_SOC_value_in_percent,max_SOC_value_in_percent):
     SOC_samples = []
     for i in range(number_of_samples):
-        num_list.append(random.randint(min_SOC_value_in_percent,max_SOC_value_in_percent))
+        SOC_samples.append(random.randint(min_SOC_value_in_percent,max_SOC_value_in_percent))
     return SOC_samples
